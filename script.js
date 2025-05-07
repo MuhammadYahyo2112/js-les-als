15dc735e40f66404d9412907a1702db1cbbbfaeb
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-05-07 12:07:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,203 @@
+function zakaz() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 45000
+            let confirmga = confirm(kirgiz + " ta qiyqiriq seti zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz1() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 79000
+            let confirmga = confirm(kirgiz + " ta Snek seti zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz2() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 34000
+            let confirmga = confirm(kirgiz + " ta Klassik set zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz3() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 45000
+            let confirmga = confirm(kirgiz + " ta Longer rings set zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz4() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 62000
+            let confirmga = confirm(kirgiz + " ta Big set zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz5() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 53000
+            let confirmga = confirm(kirgiz + " ta Lester set zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz6() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 36000
+            let confirmga = confirm(kirgiz + " ta Skul set zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz7() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 20000
+            let confirmga = confirm(kirgiz + " ta Kombo set kattasi zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz8() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 43000
+            let confirmga = confirm(kirgiz + " ta Dostlar 1x zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz9() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 79000
+            let confirmga = confirm(kirgiz + " ta Dostlar 2x zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz10() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 149000
+            let confirmga = confirm(kirgiz + " ta Dostlar 4x zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
+
+function zakaz11() {
+    let kirgiz = +prompt("Nechta zakaz qilmoqchi ekanligizni kiriting")
+
+
+    switch (kirgiz) {
+        case kirgiz:
+            let kopaytir = kirgiz * 43000
+            let confirmga = confirm(kirgiz + " ta Achchiq Dostlar 1x zakaz qildingiz " + kopaytir + " shuncha summaga togri keladi " + "\n" + "Mahsulotni zakaz qilasizmi yoki yoq")
+            let alertga = alert("Zakaz qabul qilindi")
+            break;
+
+        default:
+            alert("Siz nechta zakaz qilmoqchi ekanligizni kiritmadingiz :(")
+            break;
+    }
+}
